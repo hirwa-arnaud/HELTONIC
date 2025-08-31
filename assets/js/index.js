619e4e6b9334a584e12854b2be129ae656d980d2
Hirwa Arrnaud <hirwatrevor8@gmail.com>
2025-08-31 19:32:26 +0200

    started

---
+++ assets/js/index.js
@@ -0,0 +1,69 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+const slider = document.querySelector('.slider');
+
+function showSlide(index) {
+  currentIndex = index % totalSlides;
+
+  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+setInterval(() => {
+  showSlide(currentIndex + 1);
+}, 2000);
+function changeImage(imageSrc) {
+    document.getElementById('main-image').src = imageSrc;
+}
+
+function increaseQuantity() {
+    const quantityInput = document.getElementById('quantity');
+    quantityInput.value = parseInt(quantityInput.value) + 1;
+}
+
+function decreaseQuantity() {
+    const quantityInput = document.getElementById('quantity');
+    if (parseInt(quantityInput.value) > 1) {
+        quantityInput.value = parseInt(quantityInput.value) - 1;
+    }
+}
+ window.addEventListener("load", () => {
+      const loader = document.getElementById("loader");
+      const content = document.getElementById("content");
+      
+      loader.style.display = "none";
+      content.style.display = "block";
+    });
+       window.addEventListener("load", () => {
+      const loader = document.getElementById("loader");
+      const content = document.getElementById("content");
+      
+      loader.style.display = "none";
+      content.style.display = "block";
+    });
+    document.addEventListener("DOMContentLoaded", () => {
+  const searchInput = document.querySelector(".input input");
+  const productContainer = document.querySelector(".products");
+  const productCards = Array.from(productContainer.querySelectorAll(".card"));
+
+  searchInput.addEventListener("input", () => {
+    const query = searchInput.value.toLowerCase();
+
+    const matched = [];
+    const notMatched = [];
+
+    productCards.forEach(card => {
+      const productName = card.querySelector("p").textContent.toLowerCase();
+      const productImgAlt = card.querySelector("img").alt.toLowerCase();
+
+      if (productName.includes(query) || productImgAlt.includes(query)) {
+        matched.push(card);
+      } else {
+        notMatched.push(card);
+      }
+    });
+
+    productContainer.innerHTML = "";
+    matched.forEach(c => productContainer.appendChild(c));
+    notMatched.forEach(c => productContainer.appendChild(c));
+  });
+});
---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+function changeImage(imageSrc) {
+            document.getElementById('main-image').src = imageSrc;
+        }
+
+        function increaseQuantity() {
+            const quantityInput = document.getElementById('quantity');
+            quantityInput.value = parseInt(quantityInput.value) + 1;
+        }
+
+        function decreaseQuantity() {
+            const quantityInput = document.getElementById('quantity');
+            if (parseInt(quantityInput.value) > 1) {
+                quantityInput.value = parseInt(quantityInput.value) - 1;
+            }
+        }
+        const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+})
